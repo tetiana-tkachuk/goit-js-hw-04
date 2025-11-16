b6e6d360dc8adc26026515b8bc5d81cf9415cecb
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2025-11-16 11:35:26 +0200

    Complete task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,3 +1,15 @@
 function isEnoughCapacity(products, containerSize) {
-  return;
+  const fruits = Object.values(products);
+  let fruitsTotalNumber = 0;
+  for (const fruit of fruits) {
+    fruitsTotalNumber += fruit;
+  }
+
+  return fruitsTotalNumber <= containerSize;
 }
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
